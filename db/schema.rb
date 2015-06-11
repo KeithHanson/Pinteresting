a016b8c458e5d93f3431022fce9e03edb67746f3
Keith Hanson <keith@twinenginelabs.com>
2015-06-10 19:19:35 -0500

    Updated with Crud for Pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608235924) do
+ActiveRecord::Schema.define(version: 20150610234608) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "view_count",  default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
