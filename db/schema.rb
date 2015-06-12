6cc7cf2b7f03a4c6176b5099064a210ec8d4cfda
Keith Hanson <keith@twinenginelabs.com>
2015-06-12 18:02:48 -0500

    Adding committed work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610234608) do
+ActiveRecord::Schema.define(version: 20150611003508) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20150610234608) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
